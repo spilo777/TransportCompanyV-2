f949b9deb301521b9db30dfe6576cd5328710021
Your Name01~ <kapagiridis@gmail.com>
2023-07-28 19:14:40 +0300

    Теперь при скролле шапка будет затемняться

---
+++ logistic/assets/JS/app.js
@@ -0,0 +1,35 @@
+// Ждем, пока страница полностью загрузится, после чего выполняем код
+$(function () {
+
+    // Получаем объекты секции "intro" и шапки сайта
+    let intro = $("#intro");
+    let header = $("#header");
+
+    // Получаем высоту шапки сайта и секции "intro" сразу после загрузки страницы
+    let headerH = header.innerHeight(); // метод который высчитывает высоту любого <div> который указан в переменной.
+    let introH = intro.innerHeight();
+
+    headerScroll();
+
+    $(window).on("scroll resize", function () {
+        headerScroll();
+    });
+
+    // Функция отслеживает события прокрутки страницы и изменения размеров окна (resize)
+    function headerScroll() {
+        // При каждом событии прокрутки или изменения размеров окна, обновляем высоту шапки и секции "intro"
+        headerH = header.innerHeight(); // метод который высчитывает высоту любого <div> который указан в переменной.
+        introH = intro.innerHeight();
+
+        // Получаем текущую позицию прокрутки страницы
+        let scrollTop = $(this).scrollTop();
+
+        // Если позиция прокрутки больше или равна разнице между высотой секции "intro" и шапки, добавляем класс для темного фона шапки
+        if (scrollTop >= (introH - headerH)) {
+            header.addClass("header--dark");
+        } else {
+            // В противном случае, удаляем класс для темного фона шапки
+            header.removeClass("header--dark");
+        }
+    }
+});
