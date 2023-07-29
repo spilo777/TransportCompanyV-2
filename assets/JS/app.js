8805b12d6bc4a76cf16a72d4045fd9b9621aa688
Your Name01~ <kapagiridis@gmail.com>
2023-07-29 19:40:18 +0300

    Плавное откртие модалки и закрытие

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -76,15 +76,54 @@ $(function () {
     // Modal
     // =============================================
 
-    $("[data-modal]").on("click", function (event) {
+    $('[data-modal]').on('click', function(event) {
         event.preventDefault();
-        let modal = $(this).data("modal");
-
-        console.log(modal);
+        let modal = $(this).data('modal');
 
         $('body').addClass('no-scroll');
         $(modal).addClass('show');
 
+        setTimeout(function() {
+            $(modal).find('.modal__content').css({
+                transform: 'scale(1)',
+                opacity: '1'
+            });
+        });
     });
 
+
+    $('[data-modal-close]').on('click', function(event) {
+        event.preventDefault();
+        let modal = $(this).parents('.modal');
+        modalClose(modal);
+    });
+
+
+    $('.modal').on('click', function() {
+        let modal = $(this);
+        modalClose(modal);
+    });
+
+
+    $('.modal__content').on('click', function(event) {
+        event.stopPropagation();
+    });
+
+
+    function modalClose(modal) {
+        modal.find('.modal__content').css({
+            transform: 'scale(0.5)',
+            opacity: '0'
+        });
+
+        setTimeout(function() {
+            $('body').removeClass('no-scroll');
+            modal.removeClass('show');
+        }, 200);
+    }
+
+
+
+
 });
+
